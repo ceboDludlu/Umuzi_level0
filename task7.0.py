6fc27870f163396d72fe530ecadf02266852dab4
Nomcebo Dludlu <dludlu1550@gmail.com>
2021-07-23 12:54:29 +0200

    task7.0 commit

---
+++ task7.0.py
@@ -0,0 +1,8 @@
+#task 7.0
+w = float(input(''))
+f = float(input(''))
+
+fahrenheit = float((w - 32) * 5//9)
+celsius = float((f *9//5) + 32)
+print(fahrenheit, "fahrenheit")
+print(celsius, "celsius")
